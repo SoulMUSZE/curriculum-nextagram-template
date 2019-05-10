40225c0638a67d935c0ead52f5d22127c35412ad
Husni Wahab <mdhusni@gmail.com>
2019-05-10 17:03:41 +0800

    created images folder structure

--- models/images.py
+++ models/images.py
@@ -2,12 +2,16 @@ from models.base_model import BaseModel
 from models.user import User
 import peewee as pw
 from flask_login import UserMixin
-# from playhouse.hybrid import hybrid_property
-# from config import AWS_S3_DOMAIN, S3_BUCKET
+from playhouse.hybrid import hybrid_property
+from config import AWS_S3_DOMAIN, S3_BUCKET
 
 class Image(BaseModel, UserMixin):
     # name = pw.CharField(unique=False, null=True)
     user = pw.ForeignKeyField(User, backref='images', unique=True)
+    file_path = pw.CharField(unique=False, null=True)
+    @hybrid_property
+    def url(self):
+        return AWS_S3_DOMAIN + S3_BUCKET + '/' + self.file_path
     
  
 
