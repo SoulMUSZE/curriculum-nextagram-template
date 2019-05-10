500ca7e0be653d9bb4bfbd468879d5ea506f258d
Husni Wahab <mdhusni@gmail.com>
2019-05-10 16:48:26 +0800

    implemented user upload profile photo

--- config.py
+++ config.py
@@ -1,11 +1,9 @@
 import os
 
-import os
-
-S3_BUCKET                 = os.environ.get("S3_BUCKET_NAME")
 S3_KEY                    = os.environ.get("S3_ACCESS_KEY")
 S3_SECRET                 = os.environ.get("S3_SECRET_ACCESS_KEY")
-S3_LOCATION               = 'http://{}.s3.amazonaws.com/'.format(S3_BUCKET)
+S3_BUCKET                 = os.environ.get("S3_BUCKET_NAME")
+AWS_S3_DOMAIN             = os.environ.get("S3_AWS_URL")
 
 SECRET_KEY                = os.urandom(32)
 DEBUG                     = True
--- instagram_web/blueprints/users/views.py
+++ instagram_web/blueprints/users/views.py
@@ -1,28 +1,26 @@
+from ...util.helpers import *
 import os
 from flask import Blueprint, session, escape, render_template, Flask, request, redirect, flash, url_for
 from models.user import User
 from werkzeug.security import generate_password_hash, check_password_hash
+from werkzeug.utils import secure_filename
 from app import login_manager, app
 from flask_login import login_user, login_required, logout_user, current_user
+from config import S3_BUCKET
 
 @login_manager.user_loader
 def load_user(user_id):
     return User.get_by_id(user_id)
 
-
 users_blueprint = Blueprint('users',
                             __name__,
                             template_folder='templates')
-
-# @app.errorhandler(404)
-# def page_not_found(e):
-#     # note that we set the 404 status explicitly
-#     return render_template('404.html'), 404
 
 
 @users_blueprint.route('/new', methods=['GET'])
 def new():
     return render_template('users/new.html')
+
 
 @users_blueprint.route('/new', methods=['POST'])
 def create():
@@ -32,7 +30,8 @@ def create():
     submitted_email = request.form.get('email')
     submitted_password = request.form.get('password')
 
-    user = User.get_or_none(User.username == submitted_username and User.email == submitted_email)
+    user = User.get_or_none(
+        User.username == submitted_username and User.email == submitted_email)
 
     if user:
         flash('Account already exists. Please Login.')
@@ -50,6 +49,7 @@ def create():
             flash('Unable to create new user!')
             return render_template('users/new.html', errors=user.errors)
 
+
 @users_blueprint.route('/login', methods=['GET'])
 def login():
     return render_template('users/login.html')
@@ -64,7 +64,7 @@ def authenticate():
     user = User.get_or_none(User.username == submitted_username)
 
     if user and check_password_hash(user.hashed_password, password_to_check):
-        login_user(user) #logs in user and create cookie
+        login_user(user)  # logs in user and create cookie
         flash('Successfully Logged In')
         # return redirect(url_for('users.edit', id = user.id))
         return redirect(url_for('users.index'))
@@ -98,7 +98,7 @@ def authenticate():
 def show(username):
     user = User.get_or_none(User.username == username)
     if user:
-        return render_template('users/profile.html', user = user)
+        return render_template('users/profile.html', user=user)
     else:
         return render_template('users/404.html')
 
@@ -129,7 +129,7 @@ def update(id):
     description = request.form.get('description')
     oldPassword = request.form.get('oldPassword')
 
-    user=User.get_by_id(id)
+    user = User.get_by_id(id)
 
     if check_password_hash(user.hashed_password, oldPassword):
 
@@ -144,22 +144,24 @@ def update(id):
         if description == 'None':
             description = user.description
 
-        #update the values
+        # update the values
         u = User.update(
-             email = email, 
-             hashed_password = newPasswordHash,
-             description = description
-         ).where(User.id == user.id)
+            email=email,
+            hashed_password=newPasswordHash,
+            description=description
+        ).where(User.id == user.id)
 
         if u.execute():
             flash('New details successfully updated in database')
-            return redirect(url_for('users.show', username = user.username))
+            return redirect(url_for('users.show', username=user.username))
         else:
             flash('New details could not be updated.')
-            return redirect(url_for('users.edit', id = user.id))#do something else
+            # do something else
+            return redirect(url_for('users.edit', id=user.id))
     else:
         flash('Password could not be validated')
-        return redirect(url_for('users.show', username = user.username))#redirect or flash message
+        # redirect or flash message
+        return redirect(url_for('users.show', username=user.username))
 
 
 @users_blueprint.route('/logout', methods=["GET"])
@@ -167,6 +169,7 @@ def update(id):
 def logout():
     logout_user()
     return render_template('home.html')
+
 
 @users_blueprint.route('/<id>/edit-photo', methods=['GET'])
 @login_required
@@ -178,3 +181,53 @@ def edit_photo(id):
     else:
         return render_template('users/404.html')
 
+
+@users_blueprint.route('/<id>/update-photo', methods=['POST'])
+@login_required
+def update_photo(id):
+    
+    user = User.get_by_id(id)
+
+    if user.id == current_user.id:
+        # A
+        if "user_file" not in request.files:
+            return "No user_file key in request.files"
+        # B
+        file = request.files["user_file"]
+        """
+            These attributes are also available
+
+            file.filename               # The actual name of the file
+            file.content_type
+            file.content_length
+            file.mimetype
+
+        """
+        # C.
+        if file.filename == "":
+            return "Please select a file"
+        # D.
+        if file and allowed_file(file.filename):
+            file.filename = secure_filename(file.filename)
+            
+            if upload_file_to_s3(file, S3_BUCKET):
+                u = User.update(profile_photo_path = file.filename).where(User.id == user.id)
+                if u.execute():
+                    return redirect(url_for('users.show', username=user.username))
+                else:
+                    flash('New profile photo could not be updated.')
+                    return redirect(url_for('users.edit_photo', id=user.id))
+        else:
+            return redirect(url_for('users.edit_photo', id=user.id))
+
+# A. We check the request.files object for a user_file key. (user_file is the name of the file input on our form). If it’s not there, we return an error message.
+
+# B. If the key is in the object, we save it in a variable called file.
+
+# C. We check the filename attribute on the object and if it’s empty, it means the user sumbmitted an empty form, so we return an error message.
+
+# D. Finally we check that there is a file and that it has an allowed filetype (this is what the allowed_file function does, you can check it out in the flask docs).
+
+# If both tests pass, we sanitize the filename using the secure_filename helper function provided by the werkzeurg.security module. Next, we upload the file to our s3 bucket using our own helper function, we store the return value (ie the generated presigned url for the file) in a variable o called output. We end by returning the generated url to the user.
+
+# Note: if one of the tests fail, we just redirect the user to the home page, similar to a refresh
