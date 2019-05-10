fa9ee30a1f3cd6b195b6289a07b869312a695a0d
Husni Wahab <mdhusni@gmail.com>
2019-05-10 16:50:59 +0800

    added images model

--- models/user.py
+++ models/user.py
@@ -1,6 +1,9 @@
 from models.base_model import BaseModel
 import peewee as pw
 from flask_login import UserMixin
+from playhouse.hybrid import hybrid_property
+from config import AWS_S3_DOMAIN, S3_BUCKET
+
 
 
 class User(BaseModel, UserMixin):
@@ -8,4 +11,9 @@ class User(BaseModel, UserMixin):
     email = pw.CharField(unique=True, null=False)
     hashed_password = pw.CharField(unique=False, null=False)
     description = pw.CharField(unique=False, null=True)
+    profile_photo_path = pw.CharField(unique=False, null=True)
+    
+    @hybrid_property
+    def profile_image_url(self):
+        return AWS_S3_DOMAIN + S3_BUCKET + '/' + self.profile_photo_path
 
